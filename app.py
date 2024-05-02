154f9defc75665b8c4d8cbd962f07eaf52f3351a
movedinlights <alekudryavtsev@gmail.com>
2024-05-02 18:19:12 +0300

    gpt token setup

--- app.py
+++ app.py
@@ -15,6 +15,21 @@ load_dotenv()
 logging.basicConfig(level=logging.DEBUG)
 
 
+app.logger.setLevel(logging.INFO)
+handler = logging.FileHandler('app.log')
+app.logger.addHandler(handler)
+
+
+@app.route('/')
+def index():
+    app.logger.info('This is an INFO message')
+    app.logger.debug('This is a DEBUG message')
+    app.logger.warning('This is a WARNING message')
+    app.logger.error('This is an ERROR message')
+    app.logger.critical('This is a CRITICAL message')
+    return 'Hello, World!'
+
+
 @app.route("/compare_tm_apps")
 def compare_tm_apps():
     logging.debug("Application started, logging is configured correctly.")
