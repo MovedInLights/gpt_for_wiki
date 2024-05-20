95835234d5814275727d2a3f9b22a892f3f077bb
movedinlights <alekudryavtsev@gmail.com>
2024-05-20 23:15:56 +0300

    improve prompt

--- prompts.py
+++ prompts.py
@@ -1,28 +1,21 @@
-FINAL_MESSAGE_PROMPT = (
-    'Your trademark "{Trademark1}" - '
-    'Registered trademark "{Trademark2}" - '
-    'Similarity percentage: {SimilarityPercentage}%. '
-    'Opposition probability: {Prediction}.'
-)
-
-
 COMPARE_PROMPT = (
-    "As an expert in trademark comparison, "
-    "assess the similarity between the following two trademarks. "
-    "Based on their names, estimate the similarity percentage "
-    "and the likelihood of opposition. "
-    "Format your response as follows: {FINAL_MESSAGE_PROMPT} "
-    "Ensure your reply is concise and in Russian. "
-    "Use 'Your trademark' for the user's trademark, "
-    "'Registered trademark' for the registered trademark, "
-    "'percent' for percentage, and one of the following terms "
-    "for opposition probability: "
-    "'very low', 'low', 'average', 'high', 'very high'. "
-    "Place the names of the trademarks within curly brackets."
+    "As an expert in trademark comparison, you're tasked "
+    "with assessing the similarity between two trademarks. "
+    "Based on the names provided, estimate the similarity "
+    "percentage. "
+    "Your response should include the names of both trademarks "
+    "followed by the estimated similarity percentage, "
+    "formatted as follows: "
+    "'Ваш товарный знак Trademark 1 "
+    "- зарегистрированный товарный знак Trademark 2 "
+    "- Similarity Percentage percent'. "
+    "Ensure your reply is concise and "
+    "presented in Russian. Use the word 'процент' to denote percentage."
 )
+
 
 COMPARE_REQUEST = (
-    'Please, compare the following trademarks:\n'
-    '1. User trademark: "{clients_tm_app_name}"\n'
-    '2. Registered trademark: "{tm_app_to_compare}"\n'
+    'Please compare the following trademarks:\n'
+    '1. My Trademark: "{clients_tm_app_name}"\n'
+    '2. Registered Trademark: "{tm_app_to_compare}"\n'
 )
