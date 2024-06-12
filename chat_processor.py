a73d2a4f3b81602afdfb62f1e9e5c286a1ecdc01
movedinlights <alekudryavtsev@gmail.com>
2024-06-12 14:37:04 +0300

    fix image decoding

--- chat_processor.py
+++ chat_processor.py
@@ -1,9 +1,10 @@
 import logging
 import os
+from typing import Dict, Any
 
 from openai import OpenAI
 
-from utils import create_gpt_message, encode_image
+from utils import create_gpt_message, convert_url_image_to_base64
 
 
 # можно сделать класс и добавлять туда методы, которые могут быть выполнены
@@ -17,7 +18,7 @@ class ChatClient:
     def prepare_image(self, url: str):
         pass
 
-    def compile_messages(self, messages_draft: list):
+    def compile_messages(self, messages_draft: list) -> list[dict]:
         messages = []
         for message in messages_draft:
             logging.info(f'Working with message: {message}')
@@ -25,7 +26,7 @@ class ChatClient:
                 message_to_add = self.prepare_message(
                     role=message['role'],
                     message_type=message['type'],
-                    base64_img=encode_image(message['body']),
+                    base64_img=convert_url_image_to_base64(message['body']),
                 )
                 messages.append(message_to_add)
             else:
@@ -38,14 +39,15 @@ class ChatClient:
         return messages
 
     def prepare_message(
-        self, role: str, message_type: str, base64_img: str = "", text: str = ""
-    ):
+        self, role: str, message_type: str, base64_img: str | None = "", text: str = ""
+    ) -> Dict[str, Any]:
         if base64_img != "":
             return create_gpt_message(
                 role=role, message_type=message_type, base64_img=base64_img
             )
         if text != "":
             return create_gpt_message(role=role, message_type=message_type, text=text)
+        return {}
 
     def chat_with_gpt(self, model: str, temperature: int, messages: dict) -> str:
         api_key = os.getenv("GPT_KEY")
