860e3bef5f8ba89dabed1b982765fca027770096
movedinlights <alekudryavtsev@gmail.com>
2024-06-13 18:59:05 +0300

    fix image message

--- gpt_prompts/prompts.py
+++ gpt_prompts/prompts.py
@@ -7,9 +7,7 @@ COMPARE_PROMPT = (
 )
 
 COMPARE_REQUEST = (
-    "Please compare the following trademarks:\n"
-    '1. My Trademark: "{clients_tm_app_name}"\n'
-    '2. Registered Trademark: "{registered_tm}"\n'
+    "Please compare the following trademarks: 1. My Trademark: '{clients_tm_app_name}' 2. Registered Trademark: '{registered_tm}'"
 )
 # fmt: off
 RUPTO_TOOLKIT_TEXT = """1. Общие положения
@@ -173,20 +171,9 @@ FREESTYLE - KO’S FREESTYLE
 
 
 CONCLUSION_PROMPT = f"""
-    You are a trademark registration specialist.
-    People send you logos and you tell them if they can be registered as trademarks.
-    You will be provided with two logos (or wordmark and one logo) to compare them.
-    First one is registered as a trademark, second one is not -
-    user wants to register it.
-    You have to tell if the second logo can be registered as a trademark.
-
-    below are officially provided guideline how to compare trademarks.
-
+    You are a trademark registration specialist. People send you logos and you tell them if they can be registered as trademarks. You will be provided with two logos (or wordmark and one logo) to compare them. First one is registered as a trademark, second one is not - user wants to register it. You have to tell if the second logo can be registered as a trademark. Below are officially provided guideline how to compare trademarks.
     "{RUPTO_TOOLKIT_TEXT}"
-
     YOU SHOULD STRICTLY FOLLOW THIS GUIDELINE!!!!!
-    Provide detailed response with comparison of two trademarks and explanation,
-    based on the guideline, why it could or couldn't registered.
-    reply in russian language
+    Provide detailed response with comparison of two trademarks and explanation, based on the guideline, why it could or couldn't registered. Reply in russian language
     """
 # fmt: on
