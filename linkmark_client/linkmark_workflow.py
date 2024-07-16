a34faa711e2dcddbf1a6a1a74f8d525b4fc6f7e0
movedinlights <alekudryavtsev@gmail.com>
2024-07-16 16:13:29 +0300

    fix search type

--- linkmark_client/linkmark_workflow.py
+++ linkmark_client/linkmark_workflow.py
@@ -56,9 +56,6 @@ class BaseSearchType(ABC):
 
 
 class WordSearch(BaseSearchType):
-    def make_request(self):
-        return super().make_request()
-
     def handle_response(self, response):
         parsed_html = BeautifulSoup(response.text, 'html.parser')
 
@@ -116,75 +113,58 @@ class WordSearch(BaseSearchType):
 
 
 class NumberSearch(BaseSearchType):
-    def make_request(self):
-        return super().make_request()
-
     def handle_response(self, response):
-        parsed_html = BeautifulSoup(response.text, 'html.parser')
-        result_div = parsed_html.findAll('div', class_='result-div-item-wrapper')
-        logging.info(f'We got {len(result_div)} result(s)')
-
         trademarks = []
+        parsed_html = BeautifulSoup(response, 'html.parser')
+        items = parsed_html.select('div.result-div-item-wrapper')
 
-        for tm in result_div:
-            status_div = tm.find(
-                'div', class_='result-div-item-status tm_status status_2'
-            )
-            if not status_div:
-                status_div = tm.find(
-                    'div', class_='result-div-item-status tm_status status_4'
-                )
-            status = status_div.find('div').text.strip() if status_div else ''
+        for item in items:
+            table = item.select('div.result-div-item-v2 > table')[0]
+            try:
+                status = item.select(
+                    'div.result-div-item-status.tm_status.status_2 > div'
+                )[0].text
+            except IndexError:
+                status = item.select(
+                    'div.result-div-item-status.tm_status.status_4 > div'
+                )[0].text
 
-            link_div = tm.find('div', class_='result-div-item-action')
-            link = link_div.find('a')['href'].strip() if link_div else ''
-            table = tm.find('div', class_='result-div-item-v2').find('table')
-            rows = table.find_all('tr')
-            logging.info(f'Table HTML: {table}')
-            logging.info(f'Number of rows found: {len(rows)}')
-            doc_num = rows[0].find('td').text.strip() if len(rows) > 0 else ''
-            formatted_priority_date = (
-                rows[1].find('td').text.strip() if len(rows) > 1 else ''
-            )
+            link = item.select("div.result-div-item-action > a")[0].attrs['href']
 
-            if len(doc_num) == 10:
-                src_image_link = get_tm_app_picture(doc_num)
-                formatted_reg_date = ''
-                code_table = rows[2].find('td').text.strip() if len(rows) > 3 else ''
-                icgs_divs = code_table.split(', ')
-                icgs = [{'code': icgs_div} for icgs_div in icgs_divs]
-                owner_name_address = (
-                    rows[3].find('td').text.strip() if len(rows) >= 4 else ''
-                )
-            else:
-                src_image_link = get_tm_picture(doc_num)
-                formatted_reg_date = (
-                    rows[2].find('td').text.strip() if len(rows) > 2 else ''
-                )
-                code_table = rows[3].find('td').text.strip() if len(rows) > 3 else ''
-                icgs_divs = code_table.split(', ')
-                icgs = [{'code': icgs_div} for icgs_div in icgs_divs]
-                owner_name_address = (
-                    rows[4].find('td').text.strip() if len(rows) > 4 else ''
-                )
-
-            owner = {'name': owner_name_address}
-
-            trademark = {
-                'status': status,
-                'link': link,
-                'doc_num': doc_num,
-                'formatted_priority_date': formatted_priority_date,
-                'formatted_reg_date': formatted_reg_date,
-                'icgs': icgs,
-                'owner': owner,
-                'src_image_link': src_image_link,
+            ths, tds = table.select('th'), table.select('td')
+            result = {
+                th.text.strip().replace(':', ''): td.text.strip()
+                for th, td in zip(ths, tds)
             }
 
-            trademarks.append(trademark)
+            for old_key, new_key in key_map.items():
+                if old_key in result:
+                    if new_key == 'icgs':
+                        result[new_key] = [
+                            {'code': icgs_raw}
+                            for icgs_raw in result.pop(old_key).split(', ')
+                        ]
+                    elif new_key == 'owner':
+                        result[new_key] = {'name': result.pop(old_key)}
+                    else:
+                        result[new_key] = result.pop(old_key)
 
-        logging.info(f'Finished processing {len(trademarks)} trademark(s)!')
-        logging.info(f'Trademarks: {trademarks}')
+            result['status'] = status
+            result['link'] = link
+            try:
+                result['src_image_link'] = (
+                    get_tm_app_picture(result['tm_app_num'])
+                    if result['tm_app_num'] == 10
+                    else get_tm_picture(result['tm_app_num'])
+                )
+            except KeyError:
+                result['src_image_link'] = (
+                    get_tm_app_picture(result['doc_num'])
+                    if result['doc_num'] == 10
+                    else get_tm_picture(result['doc_num'])
+                )
+
+            trademarks.append(result)
 
         context = {
             'tm_name': self.tm_name,
@@ -198,11 +178,6 @@ class CompanySearch(BaseSearchType):
 
 
 def get_tm_picture(tm_number):
-    # https://fips.ru/ofpstorage/Doc/TM/RUNWTM/000/000/101/988/200/ТЗ-1019882-00001/00000001.jpg
-    # https://fips.ru/ofpstorage/Doc/TM/RUNWTM/000/000/101/988/200/ТЗ-1019882-00001/00000001.jpg
-    # https://fips.ru/ofpstorage/Doc/TM/RUNWTM/000/000/089/084/700/ТЗ-890847-00001/00000001.jpg
-    # https://fips.ru/ofpstorage/Doc/TM/RUNWTM/000/000/084/329/700/ТЗ-843297-00001/00000001.jpg
-
     tm_number_with_zeros = tm_number.zfill(7)
 
     url = (
@@ -229,75 +204,14 @@ def get_tm_app_picture(tm_number):
     )
 
 
-#
-# def linkmark_request(tm_name, classes_for_search):
-#     logging.info(f'Requesting linkmark for
-#     {tm_name} with classes {classes_for_search}')
-#     response = requests.post(
-#         LINKMARK_URL,
-#         data={
-#             'search': tm_name,
-#             'search_2': None,
-#             'vena-class': 'Выбрать',
-#             'search_4': None,
-#             'mktu1[]': classes_for_search,
-#             'vena_limit_subclass': '0',
-#             'vena_limit_heading': '12',
-#             'search_type': '1',
-#         },
-#         headers={'DNT': '1'},
-#     )
-#
-#     parsed_html = BeautifulSoup(response.text, 'html.parser')
-#
-#     monitoring_add_link = parsed_html.select(
-#         '#search-form > div.row > div.monitoring-add-wrapper > a'
-#     )
-#     req_id = monitoring_add_link[0].get('req-id')
-#
-#     response = requests.post(
-#         f'https://linkmark.ru/request.json/{req_id}',
-#         data={'type': 'm', 'num': '20', 'offset': '0'},
-#         headers={'DNT': '1', 'X-Requested-With': 'XMLHttpRequest'},
-#     )
-#     result = response.json()
-#
-#     tm_name_list = []
-#     for index, item in enumerate(result['res']):
-#         logging.info(
-#             f'Processing {item} {type(item)}, item words are '
-#             f'{item["words"]}, json: {json.loads(item["icgs"])}'
-#         )
-#         item['icgs'] = json.loads(item['icgs'])
-#         item['id'] = index
-#         if item['status'] != 1:
-#             tm_name_list.append(item.get('words', ''))
-#
-#     logging.info(f'Finished processing {tm_name_list} items')
-#
-#     data_for_chat = [
-#         {
-#             'status': tm.get('status', ''),
-#             'formatted_reg_date': tm.get('formatted_reg_date', ''),
-#             'link': tm.get('link', ''),
-#             'doc_num': tm.get('doc_num', ''),
-#             'src_image_link': tm.get('src_image_link', ''),
-#             'words': tm.get('words', ''),
-#             'formatted_priority_date': tm.get('formatted_priority_date', ''),
-#             'owner': tm.get('owner', ''),
-#             'code': tm.get('code', ''),
-#             'id': tm.get('id', ''),
-#         }
-#         for tm in result['res']
-#     ]
-#     data_for_chat_json = json.dumps(data_for_chat)
-#
-#     logging.info(f'Working with the results: {result["res"]}')
-#     context = {
-#         'tm_name': tm_name,
-#         'classes_for_search': classes_for_search,
-#         'results': result['res'],
-#         'data_for_chat': data_for_chat_json,
-#         'tm_name_list': tm_name_list,
-#     }
-#     return context
+key_map = {
+    'Свидетельство': 'doc_num',
+    'Приоритет': 'formatted_priority_date',
+    'Дата подачи': 'formatted_reg_date',
+    'Дата регистрации': 'formatted_reg_date',
+    'МКТУ': 'icgs',
+    'Заявленные классы': 'icgs',
+    'Правообладатель': 'owner',
+    '№ заявки': 'tm_app_num',
+    'Заявитель': 'owner',
+}
